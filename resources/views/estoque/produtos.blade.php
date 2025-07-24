61d27a25dca99c7fa5cfcfcc9c3fe656d20fdd42
feliperibeirofilg <felipearaujorib@icloud.com>
2025-07-24 19:27:49 -0300

    Adicionando front das opcoes de editar e excluir item

--- resources/views/estoque/produtos.blade.php
+++ resources/views/estoque/produtos.blade.php
@@ -1,6 +1,36 @@
 @extends('layouts.app')
 @section('content')
 
-    
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Produto:</th>
+                <th>Lote:</th>
+                <th>Validade:</th>
+                <th>Qtdde:</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
 
+        <tbody>
+            @foreach($produtos as $produto)
+            <tr>
+                <td>{{ $produto->ds_produto }}</td>
+                <td>{{ $produto->lote_produto }}</td>
+                <td>{{ $produto->dt_validade }}</td>
+                <td>{{ $produto->qtd_produto }}</td>
+                <td>
+                    <a href="#editar" class="btn btn-primary btn-sm">Editar</a>
+                </td>
+                <td>
+                    <form action="#deletar" class=""style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger btn-sm" onclick="return confirm('Tem certeza que deseja excluir esse produto?">Excluir</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,10 +7,24 @@
 
     <!-- Bootstrap CSS via CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    
+
 </head>
 <body>
-           
-    @yield('content')
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <a href="#" class="navbar-brand">Inicio</a>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Cadastrar Item</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Editar Item</a>
+                </li>
+            </div>
+        </nav>
+        <div class="container mt-05">
+            @yield('content')
+        </div>
 </body>
 </html>
